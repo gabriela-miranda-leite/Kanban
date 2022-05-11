8ba195b27aba528932596d46b3c3114ed050393c
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-11 03:26:50 -0300

    feat: create functions and structure card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,15 +1,75 @@
-import * as S from './styles';
+import { FiTrash, FiEdit, FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-interface CardProps {
-  title: string;
-  description?: string;
-}
+import { useListCards, CardProps } from "../../context/useListCards";
 
-export const Card = ({title, description}: CardProps) => {
+import * as S from "./styles";
+
+type CardDetailsProps = { card: CardProps };
+
+export const Card = ({ card }: CardDetailsProps) => {
+  const { deleteCard, changeCard } = useListCards();
+
+  const handleNext = () => {
+    return card.lista === "toDoList"
+      ? changeCard({
+          titulo: card.titulo,
+          conteudo: card.conteudo,
+          lista: "doingList",
+          id: card.id,
+        })
+      : changeCard({
+          titulo: card.titulo,
+          conteudo: card.conteudo,
+          lista: "doneList",
+          id: card.id,
+        });
+  };
+
+  const handlePrevious = () => {
+    return card.lista === "doneList"
+      ? changeCard({
+          titulo: card.titulo,
+          conteudo: card.conteudo,
+          lista: "doingList",
+          id: card.id,
+        })
+      : changeCard({
+          titulo: card.titulo,
+          conteudo: card.conteudo,
+          lista: "toDoList",
+          id: card.id,
+        });
+  };
+
   return (
     <S.Container>
-      <S.Title>{title}</S.Title>
-      <S.Description>{description}</S.Description>
+      <S.Title>{card.titulo}</S.Title>
+      <S.Description>{card.conteudo}</S.Description>
+      <S.ContainerButtons>
+        {card.lista !== "toDoList" && (
+          <S.ButtonProperties onClick={handlePrevious}>
+            <FiChevronLeft size={20} />
+          </S.ButtonProperties>
+        )}
+
+        <S.ButtonProperties>
+          <FiEdit size={20} />
+        </S.ButtonProperties>
+
+        <S.ButtonProperties
+          onClick={() => {
+            deleteCard(card.id, card.lista);
+          }}
+        >
+          <FiTrash size={20} />
+        </S.ButtonProperties>
+
+        {card.lista !== "doneList" && (
+          <S.ButtonProperties onClick={handleNext}>
+            <FiChevronRight size={20} />
+          </S.ButtonProperties>
+        )}
+      </S.ContainerButtons>
     </S.Container>
   );
 };
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,23 +1,84 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Container = styled.div`
-  padding: 1.5rem;
+const rotate = keyframes`
+  100% {
+    transform: rotate(1turn);
+  }
+
+`;
+
+export const Container = styled.button`
+  width: 100%;
+  padding: 2rem 3rem;
+
+  color: ${({ theme }) => theme.colors.text.light};
   background-color: ${({ theme }) => theme.colors.card};
+
   border-radius: 0.5rem;
+  border: none;
+
+  position: relative;
+  z-index: 2;
+  overflow: hidden;
+
+  &:hover::before {
+    content: "";
+    position: absolute;
+    z-index: -2;
+    left: -50%;
+    top: -50%;
+    width: 200%;
+    height: 200%;
+    background-color: ${({ theme }) => theme.colors.status.brown};
+
+    animation: ${rotate} 4s linear infinite;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    z-index: -1;
+    left: 0.5rem;
+    top: 0.5rem;
+    width: calc(100% - 1rem);
+    height: calc(100% - 1rem);
+    background-color: ${({ theme }) => theme.colors.card};
+  }
 `;
 
 export const Title = styled.h2`
   font-size: 1.6rem;
   font-weight: 300;
 
-  margin-bottom: 1rem;
-
-  white-space: nowrap;
+  margin-bottom: 2rem;
   width: 100%;
+
+  text-align: left;
+  white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  user-select: none;
 `;
 
 export const Description = styled(Title)`
+  font-size: 1.4rem;
   font-weight: 600;
+`;
+
+export const ContainerButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const ButtonProperties = styled.button`
+  border: none;
+
+  color: ${({ theme }) => theme.colors.text.light};
+  background-color: ${({ theme }) => theme.colors.card};
+
+  svg {
+    &:hover {
+      filter: brightness(0.5);
+    }
+  }
 `;
