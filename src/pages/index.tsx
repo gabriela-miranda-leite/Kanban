a7091efbe45dd68171221717982fa208f39db93d
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-10 21:19:32 -0300

    feat: add ListCardsProvider around home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from "next";
+
+import { ListCardsProvider } from "../context/useListCards";
 import { Home } from "./home";
 
 const Page: NextPage = () => {
-  return <Home />;
+  return (
+    <ListCardsProvider>
+      <Home />
+    </ListCardsProvider>
+  );
 };
 
 export default Page;
