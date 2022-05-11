cc2c4934feb71d40f29de9f8f804d80fde100256
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-11 12:02:05 -0300

    refactor: add onClick open modal

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,13 @@
-import { FiTrash, FiEdit, FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import {
+  FiTrash,
+  FiEdit,
+  FiChevronLeft,
+  FiChevronRight,
+  FiMaximize,
+} from "react-icons/fi";
 
 import { useListCards, CardProps } from "../../context/useListCards";
+import { useModal } from "../../context/useModal";
 
 import * as S from "./styles";
 
@@ -8,6 +15,7 @@ type CardDetailsProps = { card: CardProps };
 
 export const Card = ({ card }: CardDetailsProps) => {
   const { deleteCard, changeCard } = useListCards();
+  const { setToggleModal, toggleModal, setTypeModal, setCard } = useModal();
 
   const handleNext = () => {
     return card.lista === "toDoList"
@@ -42,34 +50,52 @@ export const Card = ({ card }: CardDetailsProps) => {
   };
 
   return (
-    <S.Container>
-      <S.Title>{card.titulo}</S.Title>
-      <S.Description>{card.conteudo}</S.Description>
-      <S.ContainerButtons>
-        {card.lista !== "toDoList" && (
-          <S.ButtonProperties onClick={handlePrevious}>
-            <FiChevronLeft size={20} />
+    <>
+      <S.Container>
+        <S.Title>{card.titulo}</S.Title>
+        <S.Description>{card.conteudo}</S.Description>
+        <S.ContainerButtons>
+          {card.lista !== "toDoList" && (
+            <S.ButtonProperties onClick={handlePrevious}>
+              <FiChevronLeft size={20} />
+            </S.ButtonProperties>
+          )}
+
+          <S.ButtonProperties
+            onClick={() => {
+              setCard(card);
+              setTypeModal("edit");
+              setToggleModal(!toggleModal);
+            }}
+          >
+            <FiEdit size={20} />
           </S.ButtonProperties>
-        )}
 
-        <S.ButtonProperties>
-          <FiEdit size={20} />
-        </S.ButtonProperties>
-
-        <S.ButtonProperties
-          onClick={() => {
-            deleteCard(card.id, card.lista);
-          }}
-        >
-          <FiTrash size={20} />
-        </S.ButtonProperties>
-
-        {card.lista !== "doneList" && (
-          <S.ButtonProperties onClick={handleNext}>
-            <FiChevronRight size={20} />
+          <S.ButtonProperties
+            onClick={() => {
+              setCard(card);
+              setTypeModal("show");
+              setToggleModal(!toggleModal);
+            }}
+          >
+            <FiMaximize size={20} />
           </S.ButtonProperties>
-        )}
-      </S.ContainerButtons>
-    </S.Container>
+
+          <S.ButtonProperties
+            onClick={() => {
+              deleteCard(card.id, card.lista);
+            }}
+          >
+            <FiTrash size={20} />
+          </S.ButtonProperties>
+
+          {card.lista !== "doneList" && (
+            <S.ButtonProperties onClick={handleNext}>
+              <FiChevronRight size={20} />
+            </S.ButtonProperties>
+          )}
+        </S.ContainerButtons>
+      </S.Container>
+    </>
   );
 };
