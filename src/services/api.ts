53935f563f6ea0e24db381080714e522a14f3593
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-11 03:14:01 -0300

    feat: add auth token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+const token =
+  typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
 const api = axios.create({
   baseURL: "http://0.0.0.0:5000",
   headers: {
+    Authorization: "Bearer " + token,
     "Content-Type": "application/json",
     Accept: "application/json",
   },
