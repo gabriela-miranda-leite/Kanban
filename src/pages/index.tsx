b2db1d51bc6018d2cf9d0060d04a9a94e21f02d4
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-10 21:03:26 -0300

    feat: call home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,8 @@
 import type { NextPage } from "next";
-import { ListCards } from "../components";
+import { Home } from "./home";
 
-import * as S from "./styles";
-
-const Home: NextPage = () => {
-  const Array = [
-    {
-      title: "Bug prioridade 5",
-      description: "Site não rodando, problemas que oifkdhskfdsh",
-    },
-    {
-      title: "Bug prioridade 3",
-      description: "Site não rodando, problemas que oifkdhskfdsh",
-    },
-  ];
-  return (
-    <S.Container>
-      <S.Title>Kanban Board</S.Title>
-
-      <S.ContainerList>
-        <ListCards title="Done" color="brown" card={Array} />
-        <ListCards title="New" color="blue" card={Array} />
-        <ListCards title="To do" color="green" card={Array} />
-        <ListCards title="Doing" color="violet" card={Array} />
-      </S.ContainerList>
-    </S.Container>
-  );
+const Page: NextPage = () => {
+  return <Home />;
 };
 
-export default Home;
+export default Page;
