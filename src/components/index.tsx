c52aeb8bb083915731ea6e861cc6ce0ef9c5440d
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-11 11:54:58 -0300

    feat: add Modal

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import { Card } from "./Card";
 import { ListCards } from "./ListCards";
 import { AddButton } from "./AddButton";
+import { Modal } from "./Modal";
 
-export { Card, ListCards, AddButton };
+export { Card, ListCards, AddButton, Modal };
