2c67cc2209f6794fa0dbfcf045e91914113338b7
Gabriela <gabrielamiranda11.10@gmail.com>
2022-05-11 03:29:58 -0300

    fix: remove provider because it's around app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from "next";
 
-import { ListCardsProvider } from "../context/useListCards";
-import { Home } from "./home";
+import Home from "./home";
 
 const Page: NextPage = () => {
-  return (
-    <ListCardsProvider>
-      <Home />
-    </ListCardsProvider>
-  );
+  return <Home />;
 };
 
 export default Page;
